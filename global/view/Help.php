e48547dd2f0167bf0f44afc0ad14a3543a9a7b64
Pipi.Y.Fu <pipifuyj@gmail.com>
2010-06-28 13:41:12 +0800

    add Plugin in global/view/Help.php

--- global/view/Help.php
+++ global/view/Help.php
@@ -14,6 +14,7 @@
 		<?php require("Header.php");?>
 		<div id="main">
 		<!--write down your content here-->
+		<H1>Introduction</H1>
 		<p>SSDG Space Science Data Grid provides a way to access the solar terrestrial space physics and planetary science data products, data services from different and distributed data nodes of Chinese Academy of Sciences，including Center for Space Science and Applied Research，Institute of Geology and Geophysics，National Astronomical Observatories and University of Science and Technology of China. The products registered in SSDG include: solar images, solar index, interplanetary magnetic field, particle flux, neutron count, geomagnetic indices, etc. </p>
 		<p>The Data Product Finder presents a list of accessible datasets on the middle position of the home page. This list that can be reduced by adding query restrictions until the desired products are found. The initial list consists of all available products. The simplest way to shorten the list is to use the text search field. For example, adding one restriction - "ACE", Data Finder will reduce the list to browse datasets associated with ACE. 
 		<p>Clicking the “Metadata” will bring up a window with detailed data product metadata. Clicking the “Document” will bring up a window with documentation and supplementary and ancillary information to assist in understanding and using the data products.</p>
@@ -21,6 +22,9 @@
 		<p>For convenience，we provide some useful data online service function, for example, reading, visualization, download and, associated data analysis and retrieval ,etc.</p>
 		<p>We value your feedback and would like you to help us improve our data grid system. Please feel free to send us your suggestion and comments to our <a href="mailto:zhengyan@cssar.ac.cn">technical support engineer</a>.</p>
 		<p>If you are Chinese user, you can use our please visit our <a href="http://drs.csdb.cn/index401Action.do?&customId=00053">SSDG </a> forum.</p>
+		<H1><A NAME="Plugin">Plugin</A></H1>
+		<p>To Install Space Science Data Grid Download Plugin, you should do as follows:</p>
+		<p>1,Download the plugin.<br>2,Make the website as a trust url in your IE explorer.<br>3,Install the plugin under the guide of README in the RAR package.</p>
 		<!--stop writing-->
 		</div>
 		<div id="Footer"><img src="images/Footer.jpg" /></div>
