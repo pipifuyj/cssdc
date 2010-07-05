ee5cbce624734ee800386e301d3a9ed68f178763
Pipi.Y.Fu <pipifuyj@gmail.com>
2010-07-05 22:28:28 +0800

    change

--- global/view/Ext.Auth.js
+++ global/view/Ext.Auth.js
@@ -1,12 +1,7 @@
 var Ext.user,Ext.userpanel;
-Ext.app.unlogtpl=new Ext.XTemplate(
-	 '<a href="http://ssdg.cssar.ac.cn:8080/login.jsp" target="_self">Login</a>',
-	 '<a href="http://auth.csdb.cn/reg01.jsp" target="_blank">Registration</a>',
-	 ''
-);
 Ext.onReady(function(){
 	Ext.user=Ext.util.Cookies.get("user");	
 	Ext.userpanel=Ext.get("userstatus");
-	if(Ext.user) Ext.userpanel.update('Welcome, Dear '+Ext.user);
-	else Ext.userpanel.update('<a href="http://ssdg.cssar.ac.cn:8080/login.jsp" target="_self">Login</a><a href="http://auth.csdb.cn/reg01.jsp" target="_blank">Registration</a>');
+	if(Ext.user==null) Ext.userpanel.update('<a href="http://ssdg.cssar.ac.cn:8080/login.jsp" target="_self">Login</a><a href="http://auth.csdb.cn/reg01.jsp" target="_blank">Registration</a>');
+	else Ext.userpanel.update('Welcome, Dear '+Ext.user);
 });
