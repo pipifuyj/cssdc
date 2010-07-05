705b1b5dfe8761b023301c83adbc596cdd095243
Pipi.Y.Fu <pipifuyj@gmail.com>
2010-07-05 23:32:32 +0800

    change

--- global/view/default.php
+++ global/view/default.php
@@ -2,6 +2,7 @@
 class defaultView extends View{
 	public function Index(){
 		if(isset($_REQUEST['userID'])) setcookie('user',$_REQUEST['userID']);
+		else setcookie('user',"")
 		require("default.Home.php");
 	}
 }
