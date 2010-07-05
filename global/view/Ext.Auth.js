7247360f1c14ea1befa540d9fa6e4ca4352a9d90
Pipi.Y.Fu <pipifuyj@gmail.com>
2010-07-05 22:41:10 +0800

    move Ext.Auth.js into Left.php

--- global/view/Ext.Auth.js
+++
@@ -1,8 +0,0 @@
-var Ext.user,Ext.userpanel;
-Ext.onReady(function(){
-	Ext.user=Ext.util.Cookies.get("user");	
-	alert(Ext.user);
-	Ext.userpanel=Ext.get("userstatus");
-	if(Ext.user==null) Ext.userpanel.update('<a href="http://ssdg.cssar.ac.cn:8080/login.jsp" target="_self">Login</a><a href="http://auth.csdb.cn/reg01.jsp" target="_blank">Registration</a>');
-	else Ext.userpanel.update('Welcome, Dear '+Ext.user);
-});
